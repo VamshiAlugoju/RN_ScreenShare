1578a949502b744d022d6252b37e24f59922f573
VamshiAlugoju <alugojuvamshi@gmail.com>
2025-05-13 18:47:09 +0530

    initail commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   ScrollView,
@@ -14,6 +14,10 @@ import {
   Text,
   useColorScheme,
   View,
+  Platform,
+  PermissionsAndroid,
+  Touchable,
+  TouchableOpacity,
 } from 'react-native';
 
 import {
@@ -23,109 +27,198 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import notifee from '@notifee/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {
+  ScreenCapturePickerView,
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  mediaDevices,
+  registerGlobals,
+} from 'react-native-webrtc';
+import {
+  createNotificationChannels,
+  showDisplayProjectionNotificaion,
+  stopForegroundService,
+} from './createNotificationChannel';
+registerGlobals();
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const requestPermission = async () => {
+    await requestCameraPermission();
+    await requestMicrophonePermission();
+    await requestNotificationPermission();
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  const [url, setUrl] = useState<null | string>(null);
+
+  const produceScreenMedia = async () => {
+    const projection_perm = await AsyncStorage.getItem('display_media_perm');
+    if (!projection_perm) {
+      const tempStream = await mediaDevices.getDisplayMedia();
+      tempStream.release();
+      await AsyncStorage.setItem('display_media_perm', JSON.stringify(true));
+    }
+
+    await showDisplayProjectionNotificaion();
+
+    const screenStream = await mediaDevices.getDisplayMedia();
+    const videoTrack = screenStream.getVideoTracks()[0];
+    const url = screenStream.toURL();
+    setUrl(url);
+    console.log('screen shareing started======>>>>>>');
+  };
+
+  useEffect(() => {
+    const iffe = async () => {
+      await createNotificationChannels();
+    };
+    iffe();
+    return () => {
+      stopForegroundService();
+    };
+  }, []);
+
+  async function resetNotification() {
+    await stopForegroundService();
+  }
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
+    <View style={{flex: 1, paddingTop: 50, gap: 20, alignItems: 'center'}}>
+      <Text style={{fontSize: 15}}> First grant necessary permissions </Text>
+      <TouchableOpacity
+        onPress={requestPermission}
+        style={{
+          backgroundColor: 'blue',
+          padding: 10,
+          alignItems: 'center',
+          width: 200,
+          alignSelf: 'center',
+        }}>
+        <Text style={{color: 'white'}}>request Permissions</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={async () => {
+          await showDisplayProjectionNotificaion();
+        }}
+        style={{
+          backgroundColor: 'blue',
+          padding: 10,
+          alignItems: 'center',
+          width: 200,
+          alignSelf: 'center',
+        }}>
+        <Text style={{color: 'white'}}>
+          show foreground notification without ScreenSharing intent (App may
+          crash){' '}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={produceScreenMedia}
+        style={{
+          backgroundColor: 'blue',
+          padding: 10,
+          alignItems: 'center',
+          width: 200,
+          alignSelf: 'center',
+        }}>
+        <Text style={{color: 'white'}}>share Screen </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={resetNotification}
+        style={{
+          backgroundColor: 'blue',
+          padding: 10,
+          alignItems: 'center',
+          width: 200,
+          alignSelf: 'center',
+        }}>
+        <Text style={{color: 'white'}}>reset notification</Text>
+      </TouchableOpacity>
+      {url && (
+        <RTCView
+          streamURL={url}
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+            width: 200,
+            height: 200,
+          }}
+          objectFit="cover"
+          zOrder={1}
+        />
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+const requestCameraPermission = async () => {
+  if (Platform.OS === 'android') {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: 'Camera Permission',
+        message: 'We need access to your camera to take photos and videos',
+        buttonPositive: 'OK',
+        buttonNegative: 'Cancel',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    } else {
+      console.log('Camera permission denied');
+    }
+    return granted;
+  }
+
+  // For iOS (using react-native-permissions)
+  if (Platform.OS === 'ios') {
+    // const permission = await request(PERMISSIONS.IOS.CAMERA);
+    // if (permission === RESULTS.GRANTED) {
+    //   console.log('Camera permission granted');
+    // } else {
+    //   consoe.log('Camera permission denied');
+    // }
+  }
+};
+
+const requestMicrophonePermission = async () => {
+  if (Platform.OS === 'android') {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      {
+        title: 'Microphone Permission',
+        message: 'We need access to your microphone to make calls',
+        buttonPositive: 'OK',
+        buttonNegative: 'Cancel',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    } else {
+      console.log('Microphone permission denied');
+    }
+    return granted;
+  }
+};
+
+const requestNotificationPermission = async () => {
+  if (Platform.OS === 'android' && Platform.Version >= 33) {
+    const settings = await notifee.getNotificationSettings();
+
+    if (settings.authorizationStatus === 0) {
+      const status = await notifee.requestPermission();
+      if (status.authorizationStatus !== 1) {
+        console.warn('Notification permission not granted');
+      }
+    }
+  }
+};
---
+++ utils/notifee.lib.ts
@@ -0,0 +1,58 @@
+import notifee, { AndroidCategory, AndroidChannel, AndroidForegroundServiceType, AndroidImportance, AndroidVisibility } from '@notifee/react-native';
+
+
+const notificationChannels: { [key: string]: AndroidChannel } = {
+  calls: {
+    id: 'calls',
+    name: 'Calls',
+    description: 'Incoming call notifications',
+    importance: AndroidImportance.HIGH,
+    sound: 'default',
+  },
+  screenCapture: {
+    id: 'screen_capture',
+    name: 'Screen Capture',
+    lights: false,
+    vibration: true,
+    importance: AndroidImportance.HIGH,
+    visibility: AndroidVisibility.PUBLIC,
+  }
+};
+
+export const createNotificationChannels = async () => {
+  await notifee.createChannel(notificationChannels.calls);
+  const channelId = await notifee.createChannel(notificationChannels.screenCapture);
+};
+
+
+export async function showDisplayProjectionNotificaion() {
+  try {
+
+    await notifee.displayNotification({
+      title: 'Screen Capture',
+      body: 'This notification will be here until you stop capturing.',
+      android: {
+        channelId: "screen_capture",
+   
+        asForegroundService: true,
+        ongoing: true,                  
+        smallIcon: 'react_native',      
+        autoCancel: false,
+      }
+    });
+  } catch (err) {
+    console.log(err)
+  };
+
+}
+
+
+
+export async function stopForegroundService() {
+  try {
+    await notifee.stopForegroundService();
+  } catch (err) {
+    // Handle Error
+  };
+
+}
